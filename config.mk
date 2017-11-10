e207fa7536d5662725306c90b403f376859fbd94
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-10 18:36:50 -0200

    fonts: Add 'GoogleSans' font family
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    
    Change-Id: I1f35c0fe543406f5da3889464377177f60cc24bc
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- config.mk
+++ config.mk
@@ -1,1 +1,10 @@
-# Empty file for now
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/fonts/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/fonts/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/fonts/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/fonts/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/fonts/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/fonts/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/fonts/fonts/google-sans.xml
