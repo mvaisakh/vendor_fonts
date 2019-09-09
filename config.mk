dd86a653c9eb05e9ad892e6c51903bb63edb224a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-09 15:01:43 -0300

    fonts: Refactor for Android 10
    Change-Id: I182e66b0d258ef2d2266f1cec363d9d56f70c92e
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- config.mk
+++ config.mk
@@ -1,10 +1,2 @@
 # Fonts
-PRODUCT_COPY_FILES += \
-    vendor/fonts/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
-    vendor/fonts/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
-    vendor/fonts/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/fonts/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
-    vendor/fonts/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    vendor/fonts/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
-
-ADDITIONAL_FONTS_FILE := vendor/fonts/fonts/google-sans.xml
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
